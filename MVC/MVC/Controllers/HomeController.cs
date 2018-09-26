e38f04f2b7b3d63b1848113f3811df5f1cf35ce7
hob201801 <sdernal2@gmail.com>
2018-09-26 22:08:26 +0300

    Добавлен проект MVC

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() {
+            return "Hello from Home/Index!";
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public string Show(Point p)
+        {
+            return $"(X: {p.X}, Y: {p.Y})";
+        }
+    }
+}
