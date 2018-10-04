b13e80f371e6bbf747cb00554885fa0df11aa480
Maksim Ermolaev <ermolmak@gmail.com>
2018-10-04 13:24:30 +0300

    Optional parameter eps for double comparison added

--- csharpTasks/task1/MyVector/MyVector/VectorExtensions.cs
+++ csharpTasks/task1/MyVector/MyVector/VectorExtensions.cs
@@ -19,15 +19,16 @@ namespace MyVector
         /// Нормализация вектора
         /// </summary>
         /// <param name="v">Вектор</param>
+        /// <param name="eps">epsilon для сравнения вещетвенных чисел</param>
         /// <returns>
         /// Вектор длины 1, сонаправленный вектору v.
-        /// Если длина v меньше 1e-9, то v считается нулевым - выбрасывается
+        /// Если длина v меньше eps, то v считается нулевым - выбрасывается
         /// исключение DivideByZeroException
         /// </returns>
-        public static Vector Normalize(this Vector v)
+        public static Vector Normalize(this Vector v, double eps = 1e-9)
         {
             var l = v.Length();
-            if (l < 1e-9)
+            if (l < eps)
             {
                 throw new DivideByZeroException("Zero sized Vector");
             }
@@ -40,15 +41,17 @@ namespace MyVector
         /// </summary>
         /// <param name="v">Первый вектор</param>
         /// <param name="u">Второй вектор</param>
+        /// <param name="eps">epsilon для сравнения вещетвенных чисел</param>
         /// <returns>
         /// Угол между векторами v и u.
-        /// Если один из векторов v и u нулевой (длина меньше 1e-9),
+        /// Если один из векторов v и u нулевой (длина меньше eps),
         /// то угол не определён - выбрасывается DivideByZeroException
         /// </returns>
-        public static double GetAngleBetween(this Vector v, Vector u)
+        public static double GetAngleBetween(this Vector v, Vector u,
+            double eps = 1e-9)
         {
-            var a = v.Normalize();
-            var b = u.Normalize();
+            var a = v.Normalize(eps);
+            var b = u.Normalize(eps);
             var p = a.DotProduct(b);
             if (p > 1)
             {
@@ -67,32 +70,34 @@ namespace MyVector
         /// </summary>
         /// <param name="v">Первый вектор</param>
         /// <param name="u">Второй вектор</param>
+        /// <param name="eps">epsilon для сравнения вещетвенных чисел</param>
         /// <returns>
         /// VectorRelation:
         /// Parallel, если параллельны
         /// Orthogonal, если перпендикулярны
         /// General, иначе
-        /// Нулевой вектор считается параллельным любому другому
+        /// Нулевой вектор (длина меньше eps) считается параллельным любому другому
         /// </returns>
-        public static VectorRelation GetRelation(this Vector v, Vector u)
+        public static VectorRelation GetRelation(this Vector v, Vector u,
+            double eps = 1e-9)
         {
             Vector a, b;
             try
             {
-                a = v.Normalize();
-                b = u.Normalize();
+                a = v.Normalize(eps);
+                b = u.Normalize(eps);
             }
             catch (DivideByZeroException)
             {
                 return VectorRelation.Parallel;
             }
 
-            if (Math.Abs(a.CrossProduct(b)) < 1e-9)
+            if (Math.Abs(a.CrossProduct(b)) < eps)
             {
                 return VectorRelation.Parallel;
             }
 
-            return Math.Abs(a.DotProduct(b)) < 1e-9
+            return Math.Abs(a.DotProduct(b)) < eps
                 ? VectorRelation.Orthogonal
                 : VectorRelation.General;
         }
@@ -102,13 +107,14 @@ namespace MyVector
         /// градусов против часовой стрелки
         /// </summary>
         /// <param name="v">Вектор</param>
+        /// <param name="eps">epsilon для сравнения вещетвенных чисел</param>
         /// <returns>
         /// Единичный вектор, повёрнутый относительно v на 90 градусов
-        /// против часовой стрелки. Для нулевого вектора выбрасывается
-        /// DivideByZeroException
+        /// против часовой стрелки. Для нулевого вектора (длина меньше eps)
+        /// выбрасывается DivideByZeroException
         /// </returns>
-        public static Vector GetOrthogonal(this Vector v) =>
-            new Vector(-v.Y, v.X).Normalize();
+        public static Vector GetOrthogonal(this Vector v, double eps = 1e-9) =>
+            new Vector(-v.Y, v.X).Normalize(eps);
 
         /// <summary>
         /// Повернуть вектор на заданный угол
