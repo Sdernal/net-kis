f187b3340987a4c2ce284ca3e7d3a70b3484c3f7
Nikita Rykov <nikita23angarsk@gmail.com>
2018-10-07 19:45:32 +0300

    Added Task1

--- csharpTasks/task1/MyVector/MyVector/Program.cs
+++ csharpTasks/task1/MyVector/MyVector/Program.cs
@@ -10,7 +10,64 @@ namespace MyVector
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");           
+            Vector a = new Vector(2, 3);
+            Vector b = new Vector(4, 5);
+            double k = 2.5;
+            Console.WriteLine($"a.ToString() = {a.ToString()}");
+            Console.WriteLine($"b.ToString() = {b.ToString()}");
+            Console.WriteLine($"{a}.Length() = {a.Length()}");
+            Console.WriteLine($"{b}.Length() = {b.Length()}");
+            Console.WriteLine($"{a}.Add({b}) = {a.Add(b)}");
+            Console.WriteLine($"{a}.Scale({k}) = {a.Scale(k)}");
+            Console.WriteLine($"{b}.Scale({k}) = {b.Scale(k)}");
+            Console.WriteLine($"{a}.DotProduct({b}) = {a.DotProduct(b)}");
+            Console.WriteLine($"{a}.CrossProduct({b}) = {a.CrossProduct(b)}");
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} * {k} = {a * k}");
+            Console.WriteLine($"{b} * {k} = {b * k}");
+            Console.WriteLine($"{k} * {a} = {k * a}");
+            Console.WriteLine($"{k} * {b} = {k * b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{b} - {a} = {b - a}");
+            Console.WriteLine($"{a} / {k} = {a / k}");
+            Console.WriteLine($"{b} / {k} = {b / k}");
+            try
+            {
+                Console.WriteLine($"{b} / 0 = {b / 0}");
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine($"Caught DivideByZeroException trying to divide {b} by 0");
+            }
+            Vector c = new Vector(4, 4);
+            Vector d = new Vector(4, -4);
+            Vector e = new Vector(-4, -4);
+            Vector f = new Vector(0, 0);
+            Vector n = new Vector(4, 0);
+            Console.WriteLine($"{c}.Normalize() = {c.Normalize()}");
+            Console.WriteLine($"{n}.Normalize() = {n.Normalize()}");
+            try
+            {
+                Console.WriteLine($"{f}.Normalize() = {f.Normalize()}");
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine($"Caught ArgumentException while calling Normalize on {f} vector");
+            }
+            Console.WriteLine($"{c}.GetAngleBetween({d}) = {c.GetAngleBetween(d)}");
+            Console.WriteLine($"{c}.GetAngleBetween({e}) = {c.GetAngleBetween(e)}");
+            Console.WriteLine($"{c}.GetAngleBetween({f}) = {c.GetAngleBetween(f)}");
+            Console.WriteLine($"{c}.GetRelation({d}) = {c.GetRelation(d)}");
+            Console.WriteLine($"{c}.GetRelation({e}) = {c.GetRelation(e)}");
+            Console.WriteLine($"{c}.GetRelation({n}) = {c.GetRelation(n)}");
+            Console.WriteLine($"{c}.GetOrthogonal() = {c.GetOrthogonal()}");
+            Console.WriteLine($"{d}.GetOrthogonal() = {d.GetOrthogonal()}");
+            Console.WriteLine($"{f}.GetOrthogonal() = {f.GetOrthogonal()}");
+            Console.WriteLine($"{n}.GetOrthogonal() = {n.GetOrthogonal()}");
+            Console.WriteLine($"{c}.Rotate(PI / 2) = {c.Rotate(Math.PI / 2)}");
+            Console.WriteLine($"{c}.Rotate(PI) = {c.Rotate(Math.PI)}");
+            Console.WriteLine($"{f}.Rotate(2.345) = {f.Rotate(2.345)}");
         }     
         
     }
--- csharpTasks/task1/MyVector/MyVector/Vector.cs
+++ csharpTasks/task1/MyVector/MyVector/Vector.cs
@@ -8,64 +8,139 @@ namespace MyVector
 {
     public struct Vector
     {
-        /* Нужно реализовать струкруру двумерный вектор (double X, double Y):
-         - Добавить коструктор
-         - Реализовать методы-заглушки:
-            - Длина, сложение векторов, умножение на число
-            - Скалярное и векторное произведение
-            - Приведение к строке: выводить (X; Y)
-         - Реализовать Операторы
-         - В Main протестировать все методы, пока без использования фреймворков: просто зовем метод, сравниваем результат и пишем в консоль
-         - К каждому полю структуры создать документацию: шаблон создается по "///" над тем, что документируем:
         /// <summary>
-        /// Привмер использования Xml documentation comments
+        /// Первая координата двумерного вектора
         /// </summary>
-        private void Foo()
+        public double X { get; private set; }
+        
+        /// <summary>
+        /// Вторая координата двумерного вектора
+        /// </summary>
+        public double Y { get; private set; }
+        
+        /// <summary>
+        /// Конструктор объекта двумерного вектора
+        /// </summary>
+        /// <param name="x">Первая координата двумерного вектора</param>
+        /// <param name="y">Вторая координата двумерного вектора</param>
+        public Vector(double x, double y)
         {
-
-        }        
-        */
+            X = x;
+            Y = y;
+        }
+        
+        /// <summary>
+        /// Рассчитывает длину двумерного вектора
+        /// </summary>
+        /// <returns>Число - длина вектора</returns>
         public double Length()
         {
-            throw new NotImplementedException();
+            return Math.Sqrt(X * X + Y * Y);
         }
 
+        /// <summary>
+        /// Прибавляет двумерный вектор к данному
+        /// </summary>
+        /// <param name="v">Двумерный вектор</param>
+        /// <returns>Вектор - результат сложения двух векторов</returns>
         public Vector Add(Vector v)
         {
-            throw new NotImplementedException();
+            return new Vector(X + v.X, Y + v.Y);
         }
 
+        /// <summary>
+        /// Домножает вектор на скаляр
+        /// </summary>
+        /// <param name="k">Скаляр</param>
+        /// <returns>Вектор - результат домножения данного вектора на скаляр</returns>
         public Vector Scale(double k)
         {
-            throw new NotImplementedException();
+            return new Vector(X * k, Y * k);
         }
 
+        /// <summary>
+        /// Рассчитывает скалярное произведение двух векторов
+        /// </summary>
+        /// <param name="v">Двумерный вектор</param>
+        /// <returns>Число - скалярное произведение двух векторов</returns>
         public double DotProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return X * v.X + Y * v.Y;
         }
 
+        /// <summary>
+        /// Рассчитывает длину векторного произведения двух векторов
+        /// </summary>
+        /// <param name="v">Двумерный вектор</param>
+        /// <returns>Число - длина векторного произведения двух векторов</returns>
         public double CrossProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return Math.Abs(X * v.Y - Y * v.X);
         }
 
+        /// <summary>
+        /// Приводит вектор к строковому представлению 
+        /// </summary>
+        /// <returns>Строка - строковое представление данного вектора</returns>
         override public string ToString()
         {
-            throw new NotImplementedException();
+            return $"({X}; {Y})";
         }
 
-        #region Operators        
-        /* В этой секции нужно реализовать следующие опрераторы 
-        - v + u, v - u (v,u - Vector)
-        - v*k, k*v, v/k (k - double)
-        - +v, -v 
-        */
-        // Такая семантика у операторов в C#
-        public static Vector operator+ (Vector v, Vector u)
+        /// <summary>
+        /// Оператор сложения двух векторов
+        /// </summary>
+        /// <param name="v1">Двумерный вектор</param>
+        /// <param name="v2">Двумерный вектор</param>
+        /// <returns>Вектор - сумма двух векторов</returns>
+        public static Vector operator +(Vector v1, Vector v2)
         {
-            throw new NotImplementedException();
-        }        
-        #endregion
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        
+        /// <summary>
+        /// Оператор вычитания двух векторов
+        /// </summary>
+        /// <param name="v1">Двумерный вектор</param>
+        /// <param name="v2">Двумерный вектор</param>
+        /// <returns>Вектор - разность двух векторов</returns>
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+        
+        /// <summary>
+        /// Оператор умножения вектора на число
+        /// </summary>
+        /// <param name="v">Двумерный вектор</param>
+        /// <param name="k">Число</param>
+        /// <returns>Вектор - результат умножения вектора на число</returns>
+        public static Vector operator *(Vector v, double k)
+        {
+            return new Vector(v.X * k, v.Y * k);
+        }
+        
+        /// <summary>
+        /// Оператор умножения числа на вектор
+        /// </summary>
+        /// <param name="k">Число</param>
+        /// <param name="v">Двумерный вектор</param>
+        /// <returns>Вектор - результат умножения числа на вектор</returns>
+        public static Vector operator *(double k, Vector v)
+        {
+            return new Vector(v.X * k, v.Y * k);
+        }
+        
+        /// <summary>
+        /// Оператор деления вектора на число
+        /// </summary>
+        /// <param name="v">Двумерный вектор</param>
+        /// <param name="k">Число</param>
+        /// <returns>Вектор - результат деления вектора на число</returns>
+        public static Vector operator /(Vector v, double k)
+        {
+            return k != 0 ? new Vector(v.X / k, v.Y / k) : throw new DivideByZeroException();
+        }
+
     }
 }
