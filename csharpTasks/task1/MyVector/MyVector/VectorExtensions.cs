ee341a2b0d3f1f95c5099464d727f798852b47d3
nik.pashmentov@ya.ru <nik.pashmentov@ya.ru>
2018-10-07 16:03:12 +0300

    Реализация VectorExtenstions

--- csharpTasks/task1/MyVector/MyVector/VectorExtensions.cs
+++ csharpTasks/task1/MyVector/MyVector/VectorExtensions.cs
@@ -19,34 +19,90 @@ namespace MyVector
     // И не забыть про документацию и тесты
     public static class VectorExtensions
     {
-        // Нормализовать вектор
+        /// <summary>
+        /// Константа для сравнения вещественных чисел
+        /// </summary>
+        private const double EPS = 1e-8;
+
+        /// <summary>
+        /// Функция, проверающая вектор на нулевой
+        /// </summary>
+        /// <param name="v">Вектор, про который мы хотим узнать ответ</param>
+        /// <returns>True, если нулевой, false иначе</returns>
+        private static bool isZero(this Vector v)
+        {
+            return Math.Abs(v.X) < EPS && Math.Abs(v.Y) < EPS;
+        }
+
+        /// <summary>
+        /// Нормализация вектора
+        /// </summary>
+        /// <param name="v">Вектор, который необходимо нормализовать</param>
+        /// <returns>Нормализованный вектор</returns>
         public static Vector Normalize(this Vector v)
         {
-            throw new NotImplementedException();
+            var length = v.Length();
+            return (v.isZero() ? v : v / length);
         }
 
-        // Получить угол между векторами в радианах
+        /// <summary>
+        /// Получения угла между двумя векторами
+        /// </summary>
+        /// <param name="v">Первый вектор</param>
+        /// <param name="u">Второй вектор</param>
+        /// <returns>Угол в радианах между векторами</returns>
         public static double GetAngleBetween(this Vector v, Vector u)
         {
-            throw new NotImplementedException();
+            if (v.isZero() || u.isZero())
+            {
+                return 0.0;
+            }
+            return Math.Acos(v.Normalize().DotProduct(u.Normalize()));
         }
 
-        // Получить отношение векторов: параллельны, перпендикулярны, остальное
+        /// <summary>
+        /// Получение отношения между векторами
+        /// </summary>
+        /// <param name="v">Первый вектор</param>
+        /// <param name="u">Второй вектор</param>
+        /// <returns>Orthogonal, Parallel или General</returns>
         public static VectorRelation GetRelation(this Vector v, Vector u)
         {
-            throw new NotImplementedException();
+            if (v.DotProduct(u) < EPS)
+            {
+                return VectorRelation.Orthogonal;
+            }
+            if (v.CrossProduct(u) < EPS)
+            {
+                return VectorRelation.Parallel;
+            }
+            return VectorRelation.General;
         }
-
-        // Еденичный ортогональный вектор данному
+        
+        /// <summary>
+        /// Вычисление единичного вектора, который ортоганален данному
+        /// </summary>
+        /// <param name="v">Вектор</param>
+        /// <returns>Единичный вектор ортоганальный заданному</returns>
         public static Vector GetOrthogonal(this Vector v)
         {
-            throw new NotImplementedException();
+            if (v.isZero())
+            {
+                return new Vector(1, 0);
+            }
+            return new Vector(-v.Y, v.X);
         }
 
         // Повернуть вектор на заданный угол
+        /// <summary>
+        /// Поворот вектора на заданный угол
+        /// </summary>
+        /// <param name="v">Вектор, который надо повернуть</param>
+        /// <param name="angle">Угол, на который надо повернуть</param>
+        /// <returns>Вектор, который повернут на заданный угол от данного</returns>
         public static Vector Rotate(this Vector v, double angle)
         {
-            throw new NotImplementedException();
+            return new Vector(v.X * Math.Cos(angle) - v.Y * Math.Sin(angle), v.X * Math.Sin(angle) + v.Y * Math.Cos(angle));
         }
     }
 }
