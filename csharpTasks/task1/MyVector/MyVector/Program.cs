99428ae219b1d8e33e93d6f74abe2301542bee05
Nikita Orekhov <nikanor97@gmail.com>
2018-10-10 12:20:20 +0300

    my task1

--- csharpTasks/task1/MyVector/MyVector/Program.cs
+++ csharpTasks/task1/MyVector/MyVector/Program.cs
@@ -10,7 +10,39 @@ namespace MyVector
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");           
+            Console.WriteLine("Hello world!");
+            Vector v1 = new Vector(3.7, 8.9);
+            Vector v2 = new Vector(2.5, 1.7);
+            Console.WriteLine($"v1: {v1.ToString()}");
+            Console.WriteLine($"v2: {v2.ToString()}");
+            Console.WriteLine($"v1 length: {v1.Length()}");
+            v1.Add(v2);
+            Console.WriteLine($"v1 add v2: {v1}");
+            v2.Scale(10);
+            Console.WriteLine($"v2 Scale 10: {v2}");
+            Console.WriteLine($"v1: {v1.ToString()}");
+            Console.WriteLine($"v2: {v2.ToString()}");
+            Console.WriteLine($"v1 dotproduct v2: {v1.DotProduct(v2)}");
+            Console.WriteLine($"v1 crossproduct v2: {v1.CrossProduct(v2)}");
+
+            Console.WriteLine($"v1 + v2: {v1 + v2}");
+            Console.WriteLine($"v1 - v2: {v1 - v2}");
+            Console.WriteLine($"v1 * 3: {v1 * 3}");
+            Console.WriteLine($"3 * v1: {3 * v1}");
+            Console.WriteLine($"v1 / 4: {v1 / 4}");
+            Console.WriteLine($"+v1: {+v1}");
+            Console.WriteLine($"-v1: {-v1}");
+
+
+
+            Console.WriteLine($"normilize: {v1.Normalize()}");
+            Console.WriteLine($"getAngleBetween: {v1.GetAngleBetween(v2)}");
+            Console.WriteLine($"getRelation: {v1.GetRelation(v2)}");
+            Console.WriteLine($"v1: {v1.ToString()}");
+            Console.WriteLine($"rotate: {v1.Rotate(3.1415)}");
+
+
+            Console.ReadLine();
         }     
         
     }
--- csharpTasks/task1/MyVector/MyVector/Vector.cs
+++ csharpTasks/task1/MyVector/MyVector/Vector.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("MyVector.test")]
 namespace MyVector
 {
     public struct Vector
     {
+
+        public double x;
+        public double y;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        public Vector(double X, double Y)
+        {
+            x = X;
+            y = Y;
+        }
         /* Нужно реализовать струкруру двумерный вектор (double X, double Y):
          - Добавить коструктор
          - Реализовать методы-заглушки:
@@ -25,34 +40,62 @@ namespace MyVector
 
         }        
         */
+
+        /// <summary>
+        /// Length of a vector
+        /// </summary>
         public double Length()
         {
-            throw new NotImplementedException();
+            return Math.Sqrt(x * x + y * y);
         }
 
+        /// <summary>
+        /// Adds v to this vector
+        /// </summary>
+        /// <param name="v"></param>
         public Vector Add(Vector v)
         {
-            throw new NotImplementedException();
+            this.x += v.x;
+            this.y += v.y;
+            return this;
         }
 
+        /// <summary>
+        /// Scales this vector into k times
+        /// </summary>
+        /// <param name="k"></param>
         public Vector Scale(double k)
         {
-            throw new NotImplementedException();
+            this.x *= k;
+            this.y *= k;
+            return this;
         }
 
+        /// <summary>
+        /// Scalar product of this vector and vector v 
+        /// </summary>
+        /// <param name="v"></param>
         public double DotProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return (this.x * v.x + this.y * v.y);
         }
 
+        /// <summary>
+        /// Cross product of this vector and vector v
+        /// </summary>
+        /// <param name="v"></param>
         public double CrossProduct(Vector v)
         {
-            throw new NotImplementedException();
+            return (this.x * v.y - this.y * v.x);
         }
 
+        /// <summary>
+        /// Transforms vector to string
+        /// </summary>
+        /// <returns></returns>
         override public string ToString()
         {
-            throw new NotImplementedException();
+            return (string)($"({Math.Round(x, 10)};{Math.Round(y, 10)})");
         }
 
         #region Operators        
@@ -64,8 +107,38 @@ namespace MyVector
         // Такая семантика у операторов в C#
         public static Vector operator+ (Vector v, Vector u)
         {
-            throw new NotImplementedException();
-        }        
+            Vector res = new Vector(v.x + u.x, v.y + u.y);
+            return res;
+        }
+        public static Vector operator -(Vector v, Vector u)
+        {
+            Vector res = new Vector(v.x - u.x, v.y - u.y);
+            return res;
+        }
+        public static Vector operator *(Vector v, double k)
+        {
+            Vector res = new Vector(v.x * k, v.y * k);
+            return res;
+        }
+        public static Vector operator *(double k, Vector v)
+        {
+            Vector res = new Vector(v.x * k, v.y * k);
+            return res;
+        }
+        public static Vector operator /(Vector v, double k)
+        {
+            Vector res = new Vector(v.x / k, v.y / k);
+            return res;
+        }
+        public static Vector operator +(Vector v)
+        {
+            return v;
+        }
+        public static Vector operator -(Vector v)
+        {
+            Vector res = new Vector(-v.x, -v.y);
+            return res;
+        }
         #endregion
     }
 }
