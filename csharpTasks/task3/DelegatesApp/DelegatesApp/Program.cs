dee058fb876f252f7152ea26ec0a57fa6acc5195
hob201801 <sdernal2@gmail.com>
2018-10-21 14:57:19 +0300

    3-е задание по C#

---
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DelegatesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Market market = new Market(new string[] { "Apple", "Google" });
+            market.AddSubscriber(new Broker());
+            for (int i = 0; i < 10; i++)
+            {
+                market.Trade();
+            }
+
+            PersonContainer container = new PersonContainer();
+            container.People = new Person[]
+            {
+                new Person {Name="John", Age=19},
+                new Person {Name="Alice", Age=21}
+            };
+
+            foreach (var p in container.GetAll(p => p.Age > 10))
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
