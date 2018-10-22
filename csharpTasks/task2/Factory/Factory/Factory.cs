dccb9dfca4569110d3bbdb792f1a56e934c90ec7
Maksim Ermolaev <ermolmak@gmail.com>
2018-10-22 16:45:32 +0300

    Factory, IFactoryProvider

---
+++ csharpTasks/task2/Factory/Factory/Factory.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Factory
+{
+    class Factory
+    {
+        public Factory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProvider()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharpTasks/task2/Factory/Factory/IFactoryProvider.cs
@@ -0,0 +1,7 @@
+namespace Factory
+{
+    interface IFactoryProvider
+    {
+        IMovable CreateObject();
+    }
+}
