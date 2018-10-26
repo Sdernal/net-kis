03a2d70ca903a01d68d81a56cda8b6ad3e7075e4
Maksim Ermolaev <ermolmak@gmail.com>
2018-10-26 19:15:47 +0300

    Car, Truck and Plane are implemented

--- csharpTasks/task2/Factory/Factory/Data.cs
+++ csharpTasks/task2/Factory/Factory/Data.cs
@@ -4,21 +4,63 @@ namespace Factory
 {
     class Car : IMovable
     {
-        public void Move()
+        public Car(string name, uint passengerSeats, uint wheels)
         {
-            throw new NotImplementedException();
+            Name = name;
+            PassengerSeats = passengerSeats;
+            Wheels = wheels;
         }
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"I'm {Name}. I have {Wheels} wheels and " +
+                              $"{PassengerSeats} passenger seats.");
+        }
+
+        protected string Name;
+        protected uint PassengerSeats;
+        protected uint Wheels;
     }
 
     class Truck : Car
     {
+        public Truck(string name, uint passengerSeats,
+            uint wheels, uint maxWeight) : base(name, passengerSeats, wheels)
+        {
+            MaxWeight = maxWeight;
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"I'm {Name}. I have {Wheels} wheels and " +
+                              $"{PassengerSeats} passenger seats.");
+            Console.WriteLine("Also, I can deliver " +
+                              $"{MaxWeight} tones of items");
+        }
+
+        protected uint MaxWeight;
     }
 
     class Plane : IMovable
     {
-        public void Move()
+        public Plane(string name, uint wings,
+            uint wheels, uint maxTakeoffWeight)
         {
-            throw new NotImplementedException();
+            Name = name;
+            Wings = wings;
+            Wheels = wheels;
+            MaxTakeoffWeight = maxTakeoffWeight;
         }
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"I'm {Name}. I have {Wings} wings and {Wheels}" +
+                              $" wheels. My MTOW is {MaxTakeoffWeight} tones");
+        }
+
+        protected string Name;
+        protected uint Wings;
+        protected uint Wheels;
+        protected uint MaxTakeoffWeight;
     }
 }
