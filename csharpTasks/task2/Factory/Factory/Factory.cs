294c321031ef4bd6be69ad862c4a129af625a0fb
Maksim Ermolaev <ermolmak@gmail.com>
2018-10-26 21:00:10 +0300

    Factory

--- csharpTasks/task2/Factory/Factory/Factory.cs
+++ csharpTasks/task2/Factory/Factory/Factory.cs
@@ -1,21 +1,33 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Factory
 {
     class Factory
     {
+        public Factory()
+        {
+            _providers = new List<IFactoryProvider<IMovable>>();
+        }
+
         public void AddProvider(IFactoryProvider<IMovable> provider)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < _providers.Count; ++i)
+            {
+                if (_providers[i].GetType() != provider.GetType()) continue;
+                _providers[i] = provider;
+                return;
+            }
+
+            _providers.Add(provider);
         }
 
         public T GetProvider<T>() where T : class, IFactoryProvider<IMovable>
         {
-            return _providers.OfType<T>().Select(provider => provider as T)
+            return _providers.OfType<T>().Select(provider => provider)
                 .FirstOrDefault();
         }
 
-        private IFactoryProvider<IMovable>[] _providers;
+        private List<IFactoryProvider<IMovable>> _providers;
     }
 }
