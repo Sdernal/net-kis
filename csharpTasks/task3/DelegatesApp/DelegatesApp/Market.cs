657abb0303e36087dc94b996d374666b658ae0bf
Maksim Ermolaev <ermolmak@gmail.com>
2018-11-05 22:11:21 +0300

    MarketEventArgs, Market.random, AddSubscriber, Trade

--- csharpTasks/task3/DelegatesApp/DelegatesApp/Market.cs
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/Market.cs
@@ -13,30 +13,42 @@ namespace DelegatesApp
     public class MarketEventArgs
     {
         /*Здесь можно добавить что угодно, цену, название компании и т.п.*/
+        public MarketEventArgs(string name, int newPrice)
+        {
+            Name = name;
+            NewPrice = newPrice;
+        }
+
+        public string Name { get; set; }
+        public int NewPrice { get; set; }
     }
 
     // Биржа, торгующая акциями
     public class Market
     {
         public delegate void MarketDelegate(object sender, MarketEventArgs args);
-        public event MarketDelegate Notify;
 
+        public event MarketDelegate Notify;
+        
         public Dictionary<string, int> Shares { get; }
+        private Random random;
+
         // Устанавливаем цены акций, хз как правильно они называются
         // В параметр передаем список имен акций
         public Market(string[] shares)
         {
             Shares = new Dictionary<string, int>();
-            Random rnd = new Random();
+            random = new Random();
             foreach (var share in shares)
             {
-                Shares.Add(share, rnd.Next(100, 1000));
+                Shares.Add(share, random.Next(100, 1000));
             }
         }
+
         // Добавляет подписчика
         public void AddSubscriber(ISubscriber sub)
         {
-            throw new NotImplementedException();
+            Notify += sub.Notified;
         }
 
         // Запускает ход торгов (устанавливает цену акциям) и уведомляет подписчиков
@@ -44,9 +56,19 @@ namespace DelegatesApp
         {
             // При реализации можно взять рандомную акцию, выставить у неё цену (тоже рандомно в некоторых пределах)
             // и проинформировать подписчиков
-            throw new NotImplementedException();
+            var enumerator = Shares.GetEnumerator();
+            var num = random.Next(Shares.Count);
+            for (var i = 0; i < num; i++)
+            {
+                enumerator.MoveNext();
+            }
+
+            Shares[enumerator.Current.Key] = random.Next(100, 1000);
+            Notify?.Invoke(this, new MarketEventArgs(enumerator.Current.Key,
+                enumerator.Current.Value));
+            enumerator.Dispose();
         }
-        
+
         /// <summary>
         /// Покупка акций
         /// </summary>
