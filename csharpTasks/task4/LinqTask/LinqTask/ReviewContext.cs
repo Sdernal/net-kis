d38c66fc2e46d8467e30c5c9dd13c5527614f0c0
pyspiced <roach.97p@gmail.com>
2018-11-25 14:15:10 +0300

    Task4 added

--- csharpTasks/task4/LinqTask/LinqTask/ReviewContext.cs
+++ csharpTasks/task4/LinqTask/LinqTask/ReviewContext.cs
@@ -9,48 +9,52 @@ namespace LinqTask
     // нужно реализовать методы-заглушки
     class ReviewContext
     {
-        private IEnumerable<Review> reviews;
+        private IEnumerable<Review> reviews_;
 
         // Инициализируется перечислением отзывов
         public ReviewContext(IEnumerable<Review> visitings)
         {
-            this.reviews = visitings;
+            reviews_ = visitings;
         }
 
         // Получение объектов отзывов у конкретного пользователя
         public IEnumerable<Review> GetUserReviews(int userId)
         {
-            throw new NotImplementedException();
+            return reviews_.Where(review => review.UserId == userId);
         }
 
         // Оценивал ли кто-нибудь фильм
         public bool IsMovieReviewed(string movieName)
         {
-            throw new NotImplementedException();
+            return reviews_.Any(review => review.Movie == movieName);
         }
 
         // Получить общие фильмы у двух пользователей
         public IEnumerable<string> CompareUsers(int user1, int user2)
         {
-            throw new NotImplementedException();
+            Func<int, IEnumerable<string>> getMovies = userId => reviews_.Where(review => review.UserId == userId)
+                                                                      .Select(review => review.Movie);
+
+            return getMovies(user1).Intersect(getMovies(user2));
         }
 
         // Получить "любимые" фильмы пользователя (оценка которых больше некоторого значения), упорядоченные по оценке
         public IEnumerable<string> GetFavouritesResources(int userId, int minimalMark = 5)
         {
-            throw new NotImplementedException();
+            return reviews_.Where(review => review.UserId == userId && review.Mark > minimalMark).Select(review => review.Movie);
         }
 
         // Получить сумму четных оценок пользователя
         public int GetUserEvenSumMarks(int userId)
         {
-            throw new NotImplementedException();
+            return reviews_.Where(review => review.UserId == userId && review.Mark % 2 == 0).Select(review => review.Mark).Sum();
         }
 
         // Получить среднюю оценку для каждого фильма
         public IEnumerable<(string, double)> GetMoviesMeanMark()
         {
-            throw new NotImplementedException();
+            return reviews_.GroupBy(review => review.Movie)
+                           .Select(group => (group.Key, group.Select(review => review.Mark).Average()));
         }    
     }
 }
