a49f49d445d558d19271036b171b342942dbfe33
Maksim Ermolaev <ermolmak@gmail.com>
2018-12-04 23:56:11 +0300

    SomeoneWhoCantIntoTrading

---
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/SomeoneWhoCantIntoTrading.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace DelegatesApp
+{
+    public class SomeoneWhoCantIntoTrading : ISubscriber
+    {
+        private int _money;
+        private int _bitcoins;
+
+        public SomeoneWhoCantIntoTrading(int money = 500)
+        {
+            _money = money;
+            _bitcoins = 0;
+        }
+
+        public void Notified(object sender, MarketEventArgs args)
+        {
+            if (!(sender is Market market))
+            {
+                Console.WriteLine("It's a spam!");
+                return;
+            }
+
+            if (args.Name != "Bitcoin")
+            {
+                Console.WriteLine("Not interesting.");
+                return;
+            }
+
+            if (args.NewPrice > args.OldPrice)
+            {
+                market.Buy(args.Name, 1, ref _bitcoins, ref _money);
+                // TODO: exception
+            }
+            else if (args.NewPrice < args.OldPrice)
+            {
+                market.Sell(args.Name, _bitcoins, ref _bitcoins, ref _money);
+                // TODO: exception
+            }
+        }
+    }
+}
