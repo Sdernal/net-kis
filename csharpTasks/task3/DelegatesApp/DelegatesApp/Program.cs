cff722599066650f841d44ee466038ce2c2dd7b6
Maksim Ermolaev <ermolmak@gmail.com>
2018-12-09 17:33:05 +0300

    task3: Program.cs filled with Market using

--- csharpTasks/task3/DelegatesApp/DelegatesApp/Program.cs
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/Program.cs
@@ -6,24 +6,36 @@ namespace DelegatesApp
     {
         static void Main(string[] args)
         {
-            Market market = new Market(new string[] { "Apple", "Google" });
-            market.AddSubscriber(new Broker());
-            for (int i = 0; i < 10; i++)
+            Market market = new Market(new[]
             {
+                "Apple", "Google", "Bitcoin"
+            });
+            Broker broker = new Broker();
+            RandomPearson randomPerson = new RandomPearson();
+            market.AddSubscriber(broker);
+            market.AddSubscriber(randomPerson);
+
+            Console.WriteLine(broker);
+            Console.WriteLine(randomPerson);
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Trade #{i + 1}\n");
                 market.Trade();
+                Console.WriteLine(broker);
+                Console.WriteLine(randomPerson);
             }
 
-            PersonContainer container = new PersonContainer();
-            container.People = new Person[]
-            {
-                new Person {Name="John", Age=19},
-                new Person {Name="Alice", Age=21}
-            };
-
-            foreach (var p in container.GetAll(p => p.Age > 10))
-            {
-                Console.WriteLine(p.ToString());
-            }
+//            PersonContainer container = new PersonContainer();
+//            container.People = new Person[]
+//            {
+//                new Person {Name="John", Age=19},
+//                new Person {Name="Alice", Age=21}
+//            };
+//
+//            foreach (var p in container.GetAll(p => p.Age > 10))
+//            {
+//                Console.WriteLine(p.ToString());
+//            }
         }
     }
 }
