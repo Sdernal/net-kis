2b1744e5a8cd48dffbc88057a8ed04a583915334
NikitaSikalov <sikalov.ns@phystech.edu>
2019-04-10 21:42:02 +0300

    Factory.cs

---
+++ csharpTasks/task2/Factory/Factory/Factory.cs
@@ -0,0 +1,30 @@
+﻿using Factory.Providers;
+using Factory.Transport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Factory
+    {
+        public Factory()
+        {
+            providers_ = new List<IProvider<IVechile>>();
+        }
+        
+        public void AddProvider(IProvider<IVechile> provider)
+        {
+            providers_.Add(provider);
+        }
+
+        public TProvider GetProvider<TProvider>() where TProvider: class, IProvider<IVechile>
+        {
+            return providers_.FirstOrDefault(item => item is TProvider) as TProvider;
+        }
+
+        private ICollection<IProvider<IVechile>> providers_;
+    }
+}
