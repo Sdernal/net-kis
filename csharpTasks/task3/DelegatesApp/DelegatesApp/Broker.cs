fd88760d8a4f1e5c3209e2ba781148b34c767752
YankinaDarya <33184577+YankinaDarya@users.noreply.github.com>
2019-04-19 21:37:12 +0300

    Update Broker.cs

--- csharpTasks/task3/DelegatesApp/DelegatesApp/Broker.cs
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/Broker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,18 +8,31 @@ namespace DelegatesApp
     public class Broker : ISubscriber
     {
         private int brokerMoney;
+        private Dictionary<string, int> shares;
         // устанавливаем деньги брокера        
         // Как хранить купленные акции - решайте сами
         public Broker(int money = 1000)
         {
             brokerMoney = money;
+            shares = new Dictionary<string, int>();
         }
 
         // Тут логика такая: с биржи прилетает обновление. Брокер может либо продать акции, либо купить  
         // При покупке или продаже брокер передает ссылку на свои счета, а биржа должна сама все высавить
         public void Notified(object sender, MarketEventArgs args)
         {
-            throw new NotImplementedException();
+            Market market = sender as Market;
+            int price_change = args.current_price - args.old_price;
+            int count = shares.ContainsKey(args.share) ? shares[args.share] : 0;
+            //покупаем акцию, если цена на нее упала
+            if (price_change < 0 && brokerMoney >= args.current_price) {
+                market.Buy(args.share, ref count, ref brokerMoney);
+            }
+            //продаем акцию, если она есть в наличии у брокера, и цена на нее возросла
+            if (price_change > 0 && shares.ContainsKey(args.Name)) {
+                market.Sell(args.share, ref count, ref brokerMoney);
+            }
+            shares[args.share] = count;
         }
     }
 }
