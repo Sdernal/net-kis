f4c441d139096189c4de46d0b81ecafa17f94096
YankinaDarya <33184577+YankinaDarya@users.noreply.github.com>
2019-04-19 21:41:30 +0300

    Add files via upload

---
+++ csharpTasks/task3/DelegatesApp/DelegatesApp/Broker_Google_love.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesApp
+{    
+    // Класс брокера, который покупает только акции Google
+    public class Broker_Google_love : ISubscriber
+    {
+        private int brokerMoney;
+        private Dictionary<string, int> shares;
+        public Broker(int money = 10000)
+        {
+            brokerMoney = money;
+            shares = new Dictionary<string, int>();
+        }
+        public void Notified(object sender, MarketEventArgs args)
+        {
+            Market market = sender as Market;
+            int count = shares.ContainsKey(args.share) ? shares[args.share] : 0;
+            if (args.share == "Google" && brokerMoney >= args.current_price) {
+                market.Buy(args.share, ref count, ref brokerMoney);
+            }
+            shares[args.share] = count;
+        }
+    }
+}
