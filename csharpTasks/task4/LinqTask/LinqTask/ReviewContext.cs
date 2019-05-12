4ba9bd9be919b36937ec0000e5be3d7b5f01ad54
NikitaSikalov <sikalov.ns@phystech.edu>
2019-05-12 22:46:42 +0300

    Update ReviewContext.cs

--- csharpTasks/task4/LinqTask/LinqTask/ReviewContext.cs
+++ csharpTasks/task4/LinqTask/LinqTask/ReviewContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -7,7 +7,7 @@ namespace LinqTask
 {
     // Контекст, который хранит список отзывов,
     // нужно реализовать методы-заглушки
-    class ReviewContext
+    public class ReviewContext
     {
         private IEnumerable<Review> reviews;
 
@@ -20,37 +20,41 @@ namespace LinqTask
         // Получение объектов отзывов у конкретного пользователя
         public IEnumerable<Review> GetUserReviews(int userId)
         {
-            throw new NotImplementedException();
+            return reviews.Where(u => u.UserId == userId);
         }
 
         // Оценивал ли кто-нибудь фильм
         public bool IsMovieReviewed(string movieName)
         {
-            throw new NotImplementedException();
+           return reviews.Where(u => u.Movie == movieName).Count() != 0;
         }
 
         // Получить общие фильмы у двух пользователей
         public IEnumerable<string> CompareUsers(int user1, int user2)
         {
-            throw new NotImplementedException();
+            return reviews
+                .Where(u => u.UserId == user1)
+                .Select(u => u.Movie)
+                .Intersect(reviews.Where(u => u.UserId == user2)
+                .Select(u => u.Movie));
         }
 
         // Получить "любимые" фильмы пользователя (оценка которых больше некоторого значения), упорядоченные по оценке
         public IEnumerable<string> GetFavouritesResources(int userId, int minimalMark = 5)
         {
-            throw new NotImplementedException();
-        }
+            return reviews.Where(u => u.UserId == userId && u.Mark > minimalMark).OrderBy(u => u.Mark).Select(u => u.Movie);
+        } 
 
         // Получить сумму четных оценок пользователя
         public int GetUserEvenSumMarks(int userId)
         {
-            throw new NotImplementedException();
+            return reviews.Where(u => u.UserId == userId && u.Mark % 2 == 0).Sum(u => u.Mark);
         }
 
         // Получить среднюю оценку для каждого фильма
         public IEnumerable<(string, double)> GetMoviesMeanMark()
         {
-            throw new NotImplementedException();
+            return reviews.GroupBy(u => u.Movie).Select(u => (u.Key, u.Average(t => t.Mark)));
         }    
     }
 }
