3e897b53d89fdda4e984184eb9391ead593544c4
Maxim <maximborodin1@gmail.com>
2019-05-23 17:20:43 +0300

    Added Calculator Task

---
+++ ASPNetTasks/Calculator/Calculator/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Calculator()
+        {
+            ViewBag.Message = "Your calculator page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Result(double value1, double value2, String operation)
+        {
+            double res = 0.0;
+            switch (operation)
+            {
+                case "+":
+                    res = value1 + value2;
+                    break;
+                case "-":
+                    res = value1 - value2;
+                    break;
+                case "*":
+                    res = value1 * value2;
+                    break;
+                case "/":
+                    if (Math.Abs(value2) < 0.000001)
+                        res = 12345.67890;
+                    else
+                    {
+                        res = value1 / value2;
+                    }
+                    break;
+                default:
+                    res = 0.0;
+                    break;
+            }
+
+//            return Content("Result is " + res);
+            ViewData["result"] = res;
+            return View();
+        }
+    }
+}
